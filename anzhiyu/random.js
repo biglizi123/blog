50ad9f63a07570221d5c057f88a9051b58e6dcef
biglizi <2120231680@qq.com>
2024-08-28 00:00:43 +0800

    Site updated: 2024-08-28 00:00:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/04/Python小屋刷题记录/","2024/08/06/hello-world/"];function toRandomPost(){
+var posts=["2024/08/06/hello-world/","2024/08/04/Python小屋刷题记录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
